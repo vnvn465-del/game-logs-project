9b7aeb39ab3a8e3e8e949b5f6b7c04786bb8ee48
dmlguyq1537 <dmlguyq1537@naver.com>
2026-07-24 23:11:19 +0900

    Swagger 패키지 추가 및 구성

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('러쉬에잇 과제 API 명세서')
+    .setDescription('게임 로그 적재 및 통계 파이프라인 API 문서입니다.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  const port = 3000;
+  await app.listen(port);
+
+  console.log(`Swagger 문서: http://localhost:${port}/api-docs`);
 }
-bootstrap();
+
+// Promise 예외 처리 추가
+bootstrap().catch((err: Error) => {
+  console.error('서버 실행 중 에러 발생:', err.message);
+});
